af2886df540c55ff8bec11d73bb3920428d044a1
Bugra Kalayci <kalaycibugra@gmail.com>
2019-03-10 23:24:50 -0400

    Page Designing

--- SlotGame/SlotGame/ViewController.swift
+++ SlotGame/SlotGame/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //MARK: Properties
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let str = "Hello, playground"
+        var x:  Int = 10
+        x=x+1
+        print(x)
+        print(str)
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
 
 }
 
---
+++ SlotGame/SlotGame/page2ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  page2ViewController.swift
+//  SlotGame
+//
+//  Created by Bugra Kalayci on 3/10/19.
+//  Copyright © 2019 Bugra Kalayci. All rights reserved.
+//
+
+import UIKit
+
+class page2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
