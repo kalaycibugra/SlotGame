2487e1ea5ae8048cd9056def35d7626bb9801d60
Bugra Kalayci <kalaycibugra@gmail.com>
2019-03-11 17:17:27 -0400

    spinning functionality

--- SlotGame/SlotGame/page2ViewController.swift
+++ SlotGame/SlotGame/page2ViewController.swift
@@ -9,14 +9,75 @@
 import UIKit
 
 class page2ViewController: UIViewController {
-
+    @IBOutlet weak var slot1: UILabel!
+    @IBOutlet weak var slot2: UILabel!
+    @IBOutlet weak var slot3: UILabel!
+    @IBOutlet weak var score: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func spin(_ sender: UIButton) {
+        let fruit=["🍎","🍏","🍐","🍊","🍋","🍌","🥥","🍉","🍇","🍓","🍈","🍒","🍑","🥭","🍍","🥝"]
+//        let randomInt = Int.random(in: 1..<5)
+        let deadtime=DispatchTime.now()+0.1
+        DispatchQueue.main.asyncAfter(deadline: deadtime){
+            self.slot1.text=fruit.randomElement()!
+            self.slot2.text=fruit.randomElement()!
+            self.slot3.text=fruit.randomElement()!
+            let deadtime=DispatchTime.now()+0.1
+            DispatchQueue.main.asyncAfter(deadline: deadtime){
+                self.slot1.text=fruit.randomElement()!
+                self.slot2.text=fruit.randomElement()!
+                self.slot3.text=fruit.randomElement()!
+                let deadtime=DispatchTime.now()+0.1
+                DispatchQueue.main.asyncAfter(deadline: deadtime){
+                    self.slot1.text=fruit.randomElement()!
+                    self.slot2.text=fruit.randomElement()!
+                    self.slot3.text=fruit.randomElement()!
+                    let deadtime=DispatchTime.now()+0.1
+                    DispatchQueue.main.asyncAfter(deadline: deadtime){
+                        self.slot1.text=fruit.randomElement()!
+                        self.slot2.text=fruit.randomElement()!
+                        self.slot3.text=fruit.randomElement()!
+                        let deadtime=DispatchTime.now()+0.1
+                        DispatchQueue.main.asyncAfter(deadline: deadtime){
+                            self.slot1.text=fruit.randomElement()!
+                            self.slot2.text=fruit.randomElement()!
+                            self.slot3.text=fruit.randomElement()!
+                            let deadtime=DispatchTime.now()+0.1
+                            DispatchQueue.main.asyncAfter(deadline: deadtime){
+                                self.slot1.text=fruit.randomElement()!
+                                self.slot2.text=fruit.randomElement()!
+                                self.slot3.text=fruit.randomElement()!
+                                let deadtime=DispatchTime.now()+0.1
+                                DispatchQueue.main.asyncAfter(deadline: deadtime){
+                                    self.slot1.text=fruit.randomElement()!
+                                    self.slot2.text=fruit.randomElement()!
+                                    self.slot3.text=fruit.randomElement()!
+                                    let deadtime=DispatchTime.now()+0.2
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        slot1.text=fruit.randomElement()!
+        slot2.text=fruit.randomElement()!
+        slot3.text=fruit.randomElement()!
+        
+    }
+//    func setValues(){
+//        self.slot1.text=fruit.randomElement()!
+//        self.slot2.text=fruit.randomElement()!
+//        self.slot3.text=fruit.randomElement()!
+//    }
+//
+    
     /*
     // MARK: - Navigation
 
